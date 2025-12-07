90d253d46e5682c4c2fafbe4670a6084fb902491
Farhaan Qazi <qazifarhaan@gmail.com>
2025-12-07 11:10:49 +0000

    runtime error fix attempt 2

--- main.py
+++ main.py
@@ -144,4 +144,4 @@ async def load_external(request: Request, external_source: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
